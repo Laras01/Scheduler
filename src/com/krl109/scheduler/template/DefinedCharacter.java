2aea6f7f4434a58d14b856d6d573d1f0bb9e1c15
RIZAL ZULFIKAR RINANDA <rzrinanda@gmail.com>
2013-06-24 10:58:25 +0700

    add new

---
+++ src/com/krl109/scheduler/template/DefinedCharacter.java
@@ -0,0 +1,35 @@
+package com.krl109.scheduler.template;
+
+public class DefinedCharacter {
+
+	String code = null;
+	 String name = null;
+	 boolean selected = false;
+	  
+	 public DefinedCharacter(String code, String name, boolean selected) {
+	  super();
+	  this.code = code;
+	  this.name = name;
+	  this.selected = selected;
+	 }
+	  
+	 public String getCode() {
+	  return code;
+	 }
+	 public void setCode(String code) {
+	  this.code = code;
+	 }
+	 public String getName() {
+	  return name;
+	 }
+	 public void setName(String name) {
+	  this.name = name;
+	 }
+	 
+	 public boolean isSelected() {
+	  return selected;
+	 }
+	 public void setSelected(boolean selected) {
+	  this.selected = selected;
+	 }
+}
